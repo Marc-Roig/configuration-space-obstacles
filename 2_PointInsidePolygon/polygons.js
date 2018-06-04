a92c04f410a6e00c09ffe68d6ae3a047b1b42eb2
Marc Roig <marc12info@gmail.com>
2018-06-04 11:51:36 +0200

    Point inside polygon finished

---
+++ 2_PointInsidePolygon/polygons.js
@@ -0,0 +1,220 @@
+function Polygon() {
+
+    this.corners = []
+    this.segments = []
+
+    this.finished = false
+    this.movable = false;
+
+    this.rCol = random(0, 255)
+    this.gCol = random(0, 255)
+    this.bCol = random(0, 255)
+
+    this.addCorner = function(p) {
+
+        this.corners.push(p)
+
+    }
+
+    this.calculateSegments = function() {
+
+        this.segments = []
+
+        for (let i = 0; i < this.corners.length; i++) {
+
+            var lastCorner
+            var firstCorner
+
+            if (i == this.corners.length - 1) { //Segment between last and first corner
+
+                lastCorner = this.corners[i] 
+                firstCorner = this.corners[0]
+
+            } else { //Segment between other corners
+
+                lastCorner = this.corners[i] 
+                firstCorner = this.corners[i+1]
+
+            }
+
+            let a = null
+            let b = null
+
+            // let a = 200 //Vertical segments have infinite slope, 1000 its enough for this usage
+            if (abs((lastCorner.x) - (firstCorner.x)) > 3) { 
+
+                a = (lastCorner.y - firstCorner.y) / (lastCorner.x - firstCorner.x)
+                b = firstCorner.y - a * firstCorner.x
+
+            }
+
+            this.segments.push(new Segment(a, b, lastCorner, firstCorner))
+
+
+        }
+
+    }
+
+    this.segmentIntersections = function(segmentIn) {
+
+        let intersections = []
+
+        for (let segment of this.segments) {
+
+            if (segment.a == null) { //segment is vertical
+
+                if (Math.min(segmentIn.p1.x, segmentIn.p2.x) <= segment.p1.x && segment.p1.x <= Math.max(segmentIn.p1.x, segmentIn.p2.x) &&
+                    Math.min(segment.p1.y, segment.p2.y) <= segmentIn.p1.y && segmentIn.p1.y <= Math.max(segment.p1.y, segment.p2.y)) {
+                    intersections.push(createVector(segment.p1.x, segment.p1.x*segmentIn.a + segmentIn.b))
+                }
+                continue
+            }
+
+            if (segmentIn.a == null) { //segment is vertical
+
+                if (Math.min(segment.p1.x, segment.p2.x) <= segmentIn.p1.x && segmentIn.p1.x <= Math.max(segment.p1.x, segment.p2.x)) {
+                    intersections.push(createVector(segment.p1.x, segment.p1.x*segment.a + segment.b))
+                }
+                continue
+            }
+
+            if (segment.a == segmentIn.a) return false 
+
+            //Solve for  -> (y = a1*x + b1) and (y = a2*x + b2)
+            let x = -(segmentIn.b-segment.b)/(segmentIn.a-segment.a)
+            // let y = p0.x*a1 + b1
+
+            //Check if x0 is in bot segments
+            if (Math.min(segmentIn.p1.x, segmentIn.p2.x) <= x && x <= Math.max(segmentIn.p1.x, segmentIn.p2.x) && 
+                Math.min(segment.p1.x, segment.p2.x) <= x && x < Math.max(segment.p1.x, segment.p2.x)) {
+
+                intersections.push(createVector(x, x*segment.a + segment.b))
+
+            }
+
+        }
+
+        return intersections
+    }
+
+    this.isPointInPolygon = function(p) {
+
+        let mouseRaySegmentX = new Segment(0, p.y, p, createVector(width, p.y))
+
+        //In one special case this calculation may be wrong, check http://alienryderflex.com/polygon/
+        let XRayIntersections = this.segmentIntersections(mouseRaySegmentX) 
+
+        if (!(XRayIntersections instanceof Array)) return false //In some cases segmentIntersections return null
+
+        push()
+        for (let intersection of XRayIntersections) {
+            stroke(255)
+            strokeWeight(15)
+            point(intersection.x, intersection.y)
+            stroke(0)
+            strokeWeight(10)
+            point(intersection.x, intersection.y)
+        }
+        pop()
+
+        if (XRayIntersections.length % 2 == 0) return false
+        else return true
+
+    }
+
+    // There may be an special case when only ray tracing
+    // with a ray trace the point may be outside and the math says otherwise 
+    // Solution sugested in alienryderflex:
+    // http://alienryderflex.com/polygon/ 
+
+    this.isSegmentIntersecting = function(segmentIn) {
+
+    }
+
+}
+
+Polygon.arePolygonsIntersecting = function(poly1, poly2) {
+
+
+
+}
+
+function Segment(a, b, p1, p2) {
+
+    this.a = a
+    this.b = b
+    this.p1 = p1
+    this.p2 = p2
+
+    this.render = function() {
+
+        line(p1.x, p1.y, p2.x, p2.y)
+
+    }
+
+}
+
+function drawPoligons() {
+
+
+    push()
+
+    for (let polygon of polygons) {
+
+        if (polygon.finished) {
+
+            //DRAW POLYGON
+            beginShape()
+
+            fill(polygon.rCol, polygon.gCol, polygon.bCol)
+
+            for (let vector of polygon.corners) {
+                vertex(vector.x, vector.y)
+            }
+
+            endShape(CLOSE)
+
+            //CHECK FOR INTERSECTIONS
+            console.log(polygon.isPointInPolygon(createVector(mouseX, mouseY)))
+
+            //MOVE POLYGON
+            if (polygon.movable) {
+
+                let XSpeed = mouseX - pmouseX
+                let YSpeed = mouseY - pmouseY
+
+                for(let corner of polygon.corners) {
+                    // console.log(corner.x)
+                    corner.x += XSpeed
+                    corner.y += YSpeed
+                }    
+                
+            }
+
+        }
+
+        else {
+
+            strokeWeight(2)
+
+            let cornersNum = polygon.corners.length
+
+            if (cornersNum > 0) {
+
+                //Draw the last line linking the last point with the mouse
+                line(mouseX, mouseY, polygon.corners[cornersNum-1].x, polygon.corners[cornersNum-1].y)
+
+                //Draw a line between a corner and the previous one
+                for (let i = 1; i < cornersNum; i++) { 
+                    line (polygon.corners[i].x, polygon.corners[i].y, polygon.corners[i-1].x, polygon.corners[i-1].y) 
+                }
+            }
+
+        }
+
+        
+    }
+
+    pop()
+
+}
--- 2_PointInsidePolygon/sketch.js
+++ 2_PointInsidePolygon/sketch.js
@@ -1,88 +1,108 @@
-let p0
-let p1
-let p2
-let p3
-let p4
+
+let polygons = []
 
 function setup() {
 
-    createCanvas(500, 500)
+    createCenteredCanvas()
 
-    p0 = createVector()
-    p1 = createVector(0, 0)
-    p2 = createVector(400, 400)
-    p3 = createVector(20, 40)
-    p4 = createVector(200, 500)
+    frameRate(20)
 
-    stroke(0)
-    if (areSegmentsIntersected(p1, p2, p3, p4)) console.log("Segments are colliding")
+    polygons.push(new Polygon())
+
+    stroke(200)
+    strokeWeight(2)
+    // if (areSegmentsIntersected(p1, p2, p3, p4)) console.log("Segments are colliding")
 
 }
 
 function draw() {
 
-    background(200)
+    background('#0e0e0e')
 
-    //First segment
-    line(p1.x, p1.y, p2.x, p2.y)
+    drawPoligons()
 
-    //Second segment
-    line(p3.x, p3.y, p4.x, p4.y)
-
-    //Mouse ray tracing
+    //Mouse ray tracing visualitzation
     line(mouseX, mouseY, width, mouseY)
 
+    drawFrame()
+}
+
+function mousePressed() {
+
+    // polygons[polygons.length-1].corners.push()
+    let moveObjects = false
+
+    //If user is not drawing any polygon, clicking on an existing polygon
+    //will make it move
+    if (polygons[polygons.length-1].corners.length == 0) {
+
+        for (let polygon of polygons) {
+            if (polygon.isPointInPolygon(createVector(mouseX, mouseY)) && polygon.finished) {
+            
+                polygon.movable = true
+                moveObjects = true
+
+            }
+        }
+    }
+
+    //If an polygon is moving don't add any corner
+    if (!moveObjects) polygons[polygons.length-1].addCorner(createVector(mouseX, mouseY))
+
+}
+
+function mouseReleased() {
+
+    for (let polygon of polygons) {
+
+        polygon.movable = false
+        polygon.calculateSegments() //Segments used to optimize calculations have to be refreshed
+
+    }
+}
+
+function keyTyped() {
+
+    if (key == 'c') {
+
+        // if(!polygons[polygons.length-1].finished) {
+            polygons.pop()
+            polygons.push(new Polygon())
+        // }
+
+    }
+
+}
+
+function doubleClicked() {
+
+    polygons[polygons.length-1].finished = true
+    polygons[polygons.length-1].corners.pop()
+    polygons[polygons.length-1].calculateSegments() //Are vertices are precalculate to optimize calculations
+
+    polygons.push(new Polygon()) //Once the polygon is finished prepare to draw another one
+
+}
+
+function createCenteredCanvas() {
+
+  createCanvas(600, 600).position(
+    (windowWidth - width) / 2,
+    (windowHeight - height) / 2
+  )
+
+}
+
+function drawFrame() {
+
+    // Frame
     push()
-    strokeWeight(5)
-
-    let numIntersections = 0
-    
-    if (areSegmentsIntersected(createVector(mouseX, mouseY), createVector(width, mouseY), p1, p2)) {
-        numIntersections++
-        point(p0.x, p0.y)
-    }
-
-    if (areSegmentsIntersected(createVector(mouseX, mouseY), createVector(width, mouseY), p3, p4)) {
-        numIntersections++
-        point(p0.x, p0.y)
-    }
-
-    // if (numIntersections % 2 != 0) console.log("Mouse is Inside polygon")
-    // else console.log("Mouse is outside polygon")
-
+    stroke(100)
+    strokeWeight(3)
+    line(0, 0, width, 0)
+    line(0, 0, 0, height)
+    line(width-1, height-1, width-1, 0)
+    line(width-1, height-1, 0, height-1)
     pop()
-
-}
-
-//Solution sugested in StackOverflow
-//https://stackoverflow.com/questions/16314069/calculation-of-intersections-between-line-segments
-
-function areSegmentsIntersected(point1, point2, point3, point4) {
-
-    let a1 = (point2.y-point1.y)/(point2.x-point1.x)
-    let b1 = point1.y - a1*point1.x
-    let a2 = (point4.y-point3.y)/(point4.x-point3.x)
-    let b2 = point3.y - a2*point3.x
-
-    //Consider parallel overlapping as not an intersection
-    //If it has to be check if intercept (b) are equal and
-    //if they are, check if 1D segments (x1, x2) and (x3, x4) 
-    //overlap. If they overlap the segments intersect
-    if (a1 == a2) return false 
-
-    //Solve for  -> (y = a1*x + b1) and (y = a2*x + b2)
-    p0.x = -(b1-b2)/(a1-a2)
-    p0.y = p0.x*a1 + b1
-
-    //Check if x0 is in bot segments
-    if (Math.min(point1.x, point2.x) < p0.x && p0.x < Math.max(point1.x, point2.x) && 
-        Math.min(point3.x, point4.x) < p0.x && p0.x < Math.max(point3.x, point4.x)) return true
-    else return false
-
-}
-
-function mouseClicked() {
-
-
 
 }
